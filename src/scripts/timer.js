28be2deae059962edc9d0fea5cea7bae731d58ce
Maria <grebinichenko.maria@gmail.com>
2019-10-19 21:54:38 +0300

    added working timer

--- src/scripts/timer.js
+++ src/scripts/timer.js
@@ -1,45 +1,71 @@
 function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
-    var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
-  
-    return {
-      x: centerX + (radius * Math.cos(angleInRadians)),
-      y: centerY + (radius * Math.sin(angleInRadians))
-    };
-  }
-  
-  function describeArc(x, y, radius, startAngle, endAngle){
-  
-      var start = polarToCartesian(x, y, radius, endAngle);
-      var end = polarToCartesian(x, y, radius, startAngle);
-  
-      var largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
-  
-      var d = [
-          "M", start.x, start.y, 
-          "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y
-      ].join(" ");
-  
-      return d;       
-  }
-  
-  window.onload = function() {
-    document.getElementById("timer__days").setAttribute("d",describeArc(100, 100, 50, 0, (59/60*360)));
-    document.getElementById("timer__days__back").setAttribute("d",describeArc(100, 100, 60, 0, 359));
-    
-    document.getElementById("timer__hours").setAttribute("d",describeArc(270, 100, 50, 0, (6/24*360)));
-    document.getElementById("timer__hours__back").setAttribute("d",describeArc(270, 100, 60, 0, 359));
+  var angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0;
 
-    document.getElementById("timer__minutes").setAttribute("d",describeArc(440, 100, 50, 0, (30/60*360)));
-    document.getElementById("timer__minutes__back").setAttribute("d",describeArc(440, 100, 60, 0, 359));
-
-    document.getElementById("timer__seconds").setAttribute("d",describeArc(610, 100, 50, 0, (30/60*360)));
-    document.getElementById("timer__seconds__back").setAttribute("d",describeArc(610, 100, 60, 0, 359));
-
-    // document.getElementById("arc1").setAttribute("d", describeArc(150, 150, 100, 0, 180));
-    // document.getElementById("arc2").setAttribute("d", describeArc(150, 150, 100, 0, 180));
-    // document.getElementById("arc3").setAttribute("d", describeArc(150, 150, 100, 0, 180));
-    // document.getElementById("arc4").setAttribute("d", describeArc(150, 150, 100, 0, 180));
+  return {
+    x: centerX + (radius * Math.cos(angleInRadians)),
+    y: centerY + (radius * Math.sin(angleInRadians))
   };
+}
 
- // https://tecadmin.net/get-current-date-time-javascript/
+function describeArc(x, y, radius, startAngle, endAngle) {
+
+  var start = polarToCartesian(x, y, radius, endAngle);
+  var end = polarToCartesian(x, y, radius, startAngle);
+
+  var largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
+
+  var d = [
+    "M", start.x, start.y,
+    "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y
+  ].join(" ");
+
+  return d;
+}
+
+function getTimeRemaining(endtime) {
+	var t = Date.parse(endtime) - Date.parse(new Date());
+	var seconds = Math.floor((t / 1000) % 60);
+	var minutes = Math.floor((t / 1000 / 60) % 60);
+	var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+	var days = Math.floor(t / (1000 * 60 * 60 * 24));
+	return {
+		'total': t,
+		'days': days,
+		'hours': hours,
+		'minutes': minutes,
+		'seconds': seconds
+	};
+}
+
+function drawTimerCircle(attributeId, x, y, timePercentage) {
+  document.getElementById(attributeId).setAttribute("d", describeArc(x, y, 50, 0, (timePercentage * 3.6)));
+  document.getElementById(`${attributeId}__back`).setAttribute("d", describeArc(x, y, 60, 0, 359));
+}
+
+function initializeClock(deadline) {
+  function updateClock() {
+    var t = getTimeRemaining(deadline);
+        document.getElementById(`timer__days__number`).innerHTML = t.days;
+        document.getElementById(`timer__hours__number`).innerHTML = t.hours;
+        document.getElementById(`timer__minutes__number`).innerHTML = t.minutes;
+        document.getElementById(`timer__seconds__number`).innerHTML = t.seconds;
+    
+        drawTimerCircle("timer__days", 85, 100, t.days / 365 * 100);
+        drawTimerCircle("timer__hours", 255, 100, t.hours / 24 * 100);
+        drawTimerCircle("timer__minutes", 425, 100, t.minutes / 60 * 100);
+        drawTimerCircle("timer__seconds", 595, 100, t.seconds / 60 * 100);
+      }
+
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  var startDate = new Date(2020, 1, 22, 11);
+  var offset = startDate.getTime() - new Date().getTime();
+  var deadline = new Date(Date.parse(new Date()) + offset);
+
+  initializeClock(deadline);
   
+  
+});
